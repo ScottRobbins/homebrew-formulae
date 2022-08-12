3a5d8aa3ad20fe5165bac37d8096b8c2d6b6985a
Scott Robbins <scott.robbins@mobiledefense.com>
2022-08-11 21:10:34 -0400

    Release 1.1.0 of Hoard

--- Formula/hoard.rb
+++ Formula/hoard.rb
@@ -1,12 +1,12 @@
 class Hoard < Formula
   desc "A simple tool to allow backing up files to a git repository and keeping them in sync across multiple computers."
   homepage "https://github.com/ScottRobbins/Hoard"
-  url "https://github.com/ScottRobbins/Hoard.git", :tag => "1.0.0"
+  url "https://github.com/ScottRobbins/Hoard.git", :tag => "1.1.0"
   head "https://github.com/ScottRobbins/Hoard.git"
 
   def install
     system "swift", "build", "-c", "release", "--disable-sandbox"
-    bin.install ".build/x86_64-apple-macosx/release/hoard"
+    bin.install ".build/release/hoard"
   end
 
   test do
